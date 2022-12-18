468e853e256cb13c28a9677016c6c86260b0935f
Agustin Gutierrez <agustin.bmed@gmail.com>
2022-12-17 19:52:37 -0500

    SWDEV-000001M: Basic math file to begin addition in C
    Why:
    
        Math is needed in C, so creating some basic math file
    
    How:
    
        First change includes addition
    
    Signed-off-by: Agustin Gutierrez <agustin.bmed@gmail.com>

---
+++ BasicMath.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 3;
+    int b = 4;
+    int result = 0;
+
+    result = a + b;
+
+    printf("Addition of a = %d, and b = %d is: %d", a, b, result);
+
+    return 0;
+}
+
+
