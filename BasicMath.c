be81526c23a79021d3f1e60e8491e151c5817579
Agustin Gutierrez <agustin.bmed@gmail.com>
2022-12-17 20:57:51 -0500

    SWDEV-000002M: Added basic operations
    Why:
    
        Needed to add basic operations like Addition, Substraction
        Multiplication, Division and Modulus.
    
    How:
    
        Added the operations required and learned to add a new lin
    
    Signed-off-by: Agustin Gutierrez <agustin.bmed@gmail.com>

--- BasicMath.c
+++ BasicMath.c
@@ -4,11 +4,23 @@ int main() {
 
     int a = 3;
     int b = 4;
-    int result = 0;
+    int resultadd = 0;
+    int resultsub = 0;
+    int resultmult = 0;
+    int resultdiv = 0;
+    int resultmod = 0;
 
-    result = a + b;
+    resultadd = a + b;
+    resultsub = a - b;
+    resultmult = a * b;
+    resultdiv = a / b;
+    resultmod = a % b;
 
-    printf("Addition of a = %d, and b = %d is: %d", a, b, result);
+    printf("Addition of a = %d, and b = %d is: %d \n", a, b, resultadd);
+    printf("Subtraction of a = %d, and b = %d is: %d \n", a, b, resultsub);
+    printf("Multiplication of a = %d, and b = %d is: %d \n", a, b, resultmult);
+    printf("Division of a = %d, and b = %d is: %d \n", a, b, resultdiv);
+    printf("Modulus of a = %d, and b = %d is: %d \n", a, b, resultmod);
 
     return 0;
 }
